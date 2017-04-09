18cc646645cee530aac1c0b6c5166d37a8cf50cd
Toby Cosh <toby@narkan.co.uk>
2017-04-09 17:29:01 +0100

    halfway through organisation refactor: GET half done

--- class/organisation.php
+++ class/organisation.php
@@ -14,27 +14,102 @@ class Organisation {
 	// Location name, address, etc.
 	// Data pulled from Locations, Categories & Opening_Hours tables
 	public $columns = array(
-		"ID"                    => null,
-		"Title"                 => "",
-		"Telephone"             => "",
-		"AlternativeTelephone"  => "",
-		"Email"                 => "",
-		"Website_URL"           => "",
-		"Information"           => "",
-		"CityOrTown"            => "",
-		"County"                => "",
-		"Postcode"              => "",
-		"Country_ID"            => 229,
-		"Latitude"              => 0.0,
-		"Longitude"             => 0.0,
-		"OpeningHours"          => ""
+		"ID"                   => null,
+		"Title"                => "",
+		"Telephone"            => "",
+		"AlternativeTelephone" => "",
+		"Email"                => "",
+		"Website_URL"          => "",
+		"Information"          => "",
+		"CityOrTown"           => "",
+		"County"               => "",
+		"Postcode"             => "",
+		"Country_ID"           => 229,
+		"Latitude"             => 0.0,
+		"Longitude"            => 0.0,
+		"OpeningHours"         => ""
 	);
 
 	// Categories / Services
 	public $services = array();
 
 
+	/**
+	 * Populate the object using the ID given, or will just initialise a new object with default properties set.
+	 */
+	function __construct( $id = null ) {
 
-	function __construct() {
+		if ( $id != null ) {
+
+			// Set this object's ID
+			$this->columns['id'] = $id;
+
+			$org = $this->get_org_from_db();
+
+			$this->populate_this_object( $org );
+		}
 	}
+
+	/**
+	 * Set the $this->column array to the data pulled from the db
+	 *    using $this->columns['id'] as the index
+	 *
+	 * @Return: Either the retrieved Organisation row, or null if db query failed
+	 */
+	function get_org_from_db() {
+		global $wpdb;
+
+		$org = null;
+
+		if ( $this->columns['id'] ) {
+			$q = $wpdb->prepare( "
+					SELECT *
+					FROM cp_Organisations
+					WHERE cp_Organisations.ID = %d
+		            ",
+				$this->columns['id'] );
+
+			$org = $wpdb->get_row( $q, ARRAY_A );
+		}
+
+		if ( ! empty( $wpdb->last_error ) ) {
+			console_debug( 'SQL error in add-edit-organisation.php, get-the-org(): ' . $wpdb->last_error );
+		}
+
+		return $org;
+	}
+
+
+	/**
+	 * Set the $this->column array using the data ($org) pulled from the db
+	 *    using $this->columns['id'] as the index
+	 *
+	 * @Return: none
+	 */
+	function populate_this_object( $org ) {
+
+		if( $org ) {
+			// DELETE: ID already populated --->  $this->columns['ID'] = $org['ID'];
+			$this->columns['Title']                = $org['Title'];
+			$this->columns['Telephone']            = $org['Telephone'];
+			$this->columns['AlternativeTelephone'] = $org['AlternativeTelephone'];
+			$this->columns['Email']                = $org['Email'];
+			$this->columns['Website_URL']          = $org['Website_URL'];
+			$this->columns['Information']          = $org['Information'];
+			$this->columns['CityOrTown']           = $org['CityOrTown'];
+			$this->columns['County']               = $org['County'];
+			$this->columns['Postcode']             = $org['Postcode'];
+			$this->columns['Country_ID']           = $org['Country_ID'];
+			$this->columns['Latitude']             = $org['Latitude'];
+			$this->columns['Longitude']            = $org['Longitude'];
+			$this->columns['OpeningHours']         = $org['OpeningHours'];
+		}
+
+
+	}
+
 }
+
+
+
+
--- inc/add-edit-organisation.php
+++ inc/add-edit-organisation.php
@@ -27,222 +27,164 @@ class cpd_AddEditOrganisation {
 	function manage_form( ) {
 		
 		// Exit if user not logged in
-		// TODO: CHECK THIS WORKS
+		// TODO: MAKE THIS WORK
 /*		if( ! current_user_can( 'administrator' ) ) {
 			cpd_not_logged_in_message();
 			return;
 		}
 */
+		// Check if an Org has been selected - ie: has a number value for $_GET['id']
+		// This covers resposnes both when selecting an org for editing, or for saving an org
+		if( isset( $_GET['id'] ) && is_int( $_GET['id'] ) ) {
+			$org = new Organisation( $_GET['id'] );
 
-		$this->get_the_org();
-
-		$this->display_form();
-	}
-	
-
-	/**
-	* Checks if there is a chosen Organisation via GET['id']
-	*.   ie: if the display list page has sent us here
-	* Returns the org as an object, else null
-	*/
-	function get_the_org() {	
-		global $wpdb;
-
-		$org = null;
-
-		// If we're coming from the display-all-orgs.php page we'll
-		//   we'll have a $_GET['id']
-		if( isset( $_GET['id'] ) && is_int( (int) $_GET['id'] ) ) {
-			$get_id = $_GET['id'];
-
-			$org = $wpdb->get_row( $wpdb->prepare( "
-				SELECT cp_Locations.ID, cp_Locations.Title, cp_Locations.Telephone, cp_Locations.AlternativeTelephone, cp_Locations.Email, cp_Locations.Website_URL, cp_Locations.Information,
-					   cp_Address.CityOrTown, cp_Address.County, cp_Address.Postcode, cp_Address.Country_ID, cp_Address.Latitude, cp_Address.Longitude,
-					   cp_OpeningHours.OpeningHours
-				FROM cp_Locations
-				INNER JOIN cp_Address ON cp_Address.ID = cp_Locations.Address_ID
-				LEFT JOIN cp_OpeningHours ON cp_OpeningHours.Locations_ID = cp_Locations.ID
-				WHERE cp_Locations.ID = %d
-	            ",
-				$get_id ),
-				ARRAY_A
-			);
-
-			if( ! empty( $wpdb->last_error ) ) {
-				console_debug( 'SQL error in add-edit-organisation.php, get-the-org(): ' . $wpdb->last_error );
-			}
-
+		} else {
+			$org = new Organisation();
 		}
 
-		// If the $org array has contents, assign them to the Location object
-		if( $org ) {
-			$this->populate_org_object( $org );
-		}
-	}
-	
-
-	/*
-	 * Takes the data array from the query in get_the_org() and inserts it into the Location object
-	 * If the array is not set / null, it'll just create a new empty Location object
-	 */
-	function populate_org_object( $org ) {
-
-		$this->org_object->columns['ID']                   = $org['ID'];
-		$this->org_object->columns['Title']                = $org['Title'];
-		$this->org_object->columns['Telephone']            = $org['Telephone'];
-		$this->org_object->columns['AlternativeTelephone'] = $org['AlternativeTelephone'];
-		$this->org_object->columns['Email']                = $org['Email'];
-		$this->org_object->columns['Website_URL']          = $org['Website_URL'];
-		$this->org_object->columns['Information']          = $org['Information'];
-		$this->org_object->columns['CityOrTown']           = $org['CityOrTown'];
-		$this->org_object->columns['County']               = $org['County'];
-		$this->org_object->columns['Postcode']             = $org['Postcode'];
-		$this->org_object->columns['Country_ID']           = $org['Country_ID'];
-		$this->org_object->columns['Latitude']             = $org['Latitude'];
-		$this->org_object->columns['Longitude']            = $org['Longitude'];
-		$this->org_object->columns['OpeningHours']         = $org['OpeningHours'];
-
-
+		$this->display_form( $org );
 	}
 
-	
+
 	/*
 	 * Displays the Add / Edit form. Takes data from the Location object
 	 */
-	function display_form() {
-	
-	// TODO: textarea for info & address is maxlength=1000. Ensure db column is varchar(1000) too
-echo esc_url( admin_url( 'admin-post.php' ) );		?>
-	<form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="POST">
-	  <div class="row">
-	    <div class="medium-6 columns">
-	      <label>Organisation name
-	        <input type="text" name="name" value="<?php echo esc_attr( $this->org_object->columns['Title'] ); ?>">
-	      </label>
-	    </div>
-	    <div class="medium-6 columns">
-	      <label>Telephone
-	        <input type="text" name="telephone" value="<?php echo esc_attr( $this->org_object->columns['Telephone'] ); ?>">
-	      </label>
-	    </div>
-	    <div class="medium-6 columns">
-	      <label>Telephone alternative number
-	        <input type="text" name="alternativetelephone" value="<?php echo esc_attr( $this->org_object->columns['AlternativeTelephone'] ); ?>">
-	      </label>
-	    </div>
-		<div class="medium-6 columns">
-	      <label>Email
-	        <input type="email" name="email" value="<?php echo esc_attr( $this->org_object->columns['Email'] ); ?>">
-	      </label>
-	    </div>
-		<div class="medium-6 columns">
-	      <label>Website URL
-	        <input type="text" name="website_url" value="<?php echo esc_url( $this->org_object->columns['Website_URL'] ); ?>">
-	      </label>
-	    </div>
+	function display_form( $org ) {
 
-		<div class="medium-6 columns">
-	      <label>Address
-	        <textarea name="address" rows="4" maxlength="1000">NOT USED</textarea>
-	      </label>
-	    </div>
-		  <div class="medium-6 columns">
-			  <label>Location
-				  <-- ie: parish -->
-				  <input type="text" name="location" value="NOT USED" disabled>
-			  </label>
-		  </div>
-		  <div class="medium-6 columns">
-			  <label>Town / City
-				  <input type="text" name="cityortown" value="<?php echo esc_attr( $this->org_object->columns['CityOrTown'] ); ?>" disabled>
-			  </label>
-		  </div>
-		  <div class="medium-6 columns">
-			  <label>County
-				  <input type="text" name="county" value="<?php echo esc_attr( $this->org_object->columns['County'] ); ?>" disabled>
-			  </label>
-		  </div>
-	    <div class="medium-6 columns">
-	      <label>Postcode
-	        <input type="text" name="postcode" value="<?php echo esc_attr( $this->org_object->columns['Postcode'] ); ?>">
-	      </label>
-	    </div>
-		  <div class="medium-6 columns">
-			  <label>Country
-				  <input type="text" name="country_id" value="<?php echo esc_attr( $this->org_object->columns['Country_ID'] ); ?>">
-			  </label>
-		  </div>
-	    <div class="medium-6 columns">
-	      <label>Latitude
-	        <input type="text" name="latitude" value="<?php echo esc_attr( $this->org_object->columns['Latitude'] ); ?>" disabled>
-	      </label>
-	    </div>
-		<div class="medium-6 columns">
-	      <label>Longitude
-	        <input type="text" name="longitude" value="<?php echo esc_attr( $this->org_object->columns['Longitude'] ); ?>" disabled>
-	      </label>
-	    </div>
-		  <div class="medium-6 columns">
-			  <label>Opening Hours
-				  <textarea name="openinghours"><?php echo esc_textarea( $this->org_object->columns['OpeningHours'] ); ?></textarea>
-			  </label>
-		  </div>
+		// TODO: textarea for info & address is maxlength=1000. Ensure db column is varchar(1000) too
+		?>
+
+		<form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="POST">
+			<div class="row">
+				<div class="medium-6 columns">
+					<label>Organisation name
+						<input type="text" name="name" value="<?php echo esc_attr( $org->columns['Title'] ); ?>">
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Telephone
+						<input type="text" name="telephone" value="<?php echo esc_attr( $org->columns['Telephone'] ); ?>">
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Telephone alternative number
+						<input type="text" name="alternativetelephone" value="<?php echo esc_attr( $org->columns['AlternativeTelephone'] ); ?>">
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Email
+						<input type="email" name="email" value="<?php echo esc_attr( $org->columns['Email'] ); ?>">
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Website URL
+						<input type="text" name="website_url" value="<?php echo esc_url( $org->columns['Website_URL'] ); ?>">
+					</label>
+				</div>
+
+				<div class="medium-6 columns">
+					<label>Address
+						<textarea name="address" rows="4" maxlength="1000">NOT USED</textarea>
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Location
+						<-- ie: parish -->
+						<input type="text" name="location" value="NOT USED" disabled>
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Town / City
+						<input type="text" name="cityortown" value="<?php echo esc_attr( $org->columns['CityOrTown'] ); ?>" disabled>
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>County
+						<input type="text" name="county" value="<?php echo esc_attr( $org->columns['County'] ); ?>" disabled>
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Postcode
+						<input type="text" name="postcode" value="<?php echo esc_attr( $org->columns['Postcode'] ); ?>">
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Country
+						<input type="text" name="country_id" value="<?php echo esc_attr( $org->columns['Country_ID'] ); ?>">
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Latitude
+						<input type="text" name="latitude" value="<?php echo esc_attr( $org->columns['Latitude'] ); ?>" disabled>
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Longitude
+						<input type="text" name="longitude" value="<?php echo esc_attr( $org->columns['Longitude'] ); ?>" disabled>
+					</label>
+				</div>
+				<div class="medium-6 columns">
+					<label>Opening Hours
+						<textarea name="openinghours"><?php echo esc_textarea( $org->columns['OpeningHours'] ); ?></textarea>
+					</label>
+				</div>
 
 
+				<?php // Get a list of all the services
+				$services = $this->get_service_list();
 
-		<?php // Get a list of all the services
-		$services = $this->get_service_list();
-		
-		if( ! $services || empty( $services ) ) {
-			// Error retrieving Services
-			console_debug( 'There was a problem retrieving the services from the database' );
-		} else { 	
-	
-			// Outputs the services checkboxes, ticking any that are selected for this organisation
-			// TODO: Need to find out which are selected for this organisation if we're editing it
-			?>
-			<fieldset class="services-checkboxes">
-				<legend>Services</legend>
-				
-				<?php foreach( $services as $service ) {
-					$id     = $service['ID'];
-					$title  = $service['Title'];
-					
-					echo '<input id="service-' . $id . '" type="checkbox" name="services[]" value="' . $id .'"> <label for="service-' . $id .'">' . $title . '</label>';
-				}	?>
-				
-			</fieldset>
-			
-		<?php }  // endif ?>
-		
-		<div class="medium-6 columns">
-	      <label>Other Information
-	        <textarea name="information" rows="4" maxlength="1000"><?php echo esc_textarea( $this->org_object->columns['Information'] ); ?></textarea>
-	      </label>
-	    </div>
+				if ( ! $services || empty( $services ) ) {
+					// Error retrieving Services
+					console_debug( 'There was a problem retrieving the services from the database' );
+				} else {
 
-			
-		<?php // Hidden field of id. Will be 'null' if a new record.
+					// Outputs the services checkboxes, ticking any that are selected for this organisation
+					// TODO: Need to find out which are selected for this organisation if we're editing it
+					?>
+					<fieldset class="services-checkboxes">
+						<legend>Services</legend>
 
-		if( $this->org_object->columns['ID'] == null ) {
-			$the_id = 'null';
-		} else {
-			$the_id = $this->org_object->columns['ID'];
-		}
+						<?php foreach ( $services as $service ) {
+							$id    = $service['ID'];
+							$title = $service['Title'];
 
-		echo '<input type="hidden" name="id" value="' . esc_attr( $the_id ) . '" />';		?>
-		
-		<?php // Set the action handler for wordpress ?>
-		<input type="hidden" name="action" value="add_edit_org" />
-	
-		
-		<div class="medium-6 columns">
-			<input class="button" type="submit" name="submit" value="Save">
-			<input class="button" type="submit" name="submit" value="Cancel" formnovalidate>
-		</div>
-	  </div>	
-	  
+							echo '<input id="service-' . $id . '" type="checkbox" name="services[]" value="' . $id . '"> <label for="service-' . $id . '">' . $title . '</label>';
+						} ?>
+
+					</fieldset>
+
+				<?php }  // endif ?>
+
+				<div class="medium-6 columns">
+					<label>Other Information
+						<textarea name="information" rows="4" maxlength="1000"><?php echo esc_textarea( $org->columns['Information'] ); ?></textarea>
+					</label>
+				</div>
+
+
+				<?php // Hidden field of id. Will be 'null' if a new record.
+
+				if ( $org->columns['ID'] == null ) {
+					$the_id = 'null';
+				} else {
+					$the_id = $org->columns['ID'];
+				}
+
+				echo '<input type="hidden" name="id" value="' . esc_attr( $the_id ) . '" />'; ?>
+
+				<?php // Set the action handler for wordpress ?>
+				<input type="hidden" name="action" value="add_edit_org"/>
+
+
+				<div class="medium-6 columns">
+					<input class="button" type="submit" name="submit" value="Save">
+					<input class="button" type="submit" name="submit" value="Cancel" formnovalidate>
+				</div>
+			</div>
+		</form>
+
 	<?php }
+
 	
 	/**
 	* Returns an array of all services from the db
